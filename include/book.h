07f8a5b5d4c228e8d822556266edaf3424b85fb6
hmo2316799-coder <hmo2316799@gmail.com>
2026-04-14 14:13:28 +0300

    Add files via upload

--- include/book.h
+++ include/book.h
@@ -1,10 +1,33 @@
 #ifndef BOOK_H
 #define BOOK_H
 
+#include <stdbool.h>
+
+// Define the struct
 typedef struct {
-    int id;
-    char title[100];
-    int isBorrowed;
+    char* title;
+    char* author;
+    char* ISBN;
+    int year;
+    bool available;
 } Book;
+
+// Lifecycle
+Book* createBook(const char* title, const char* author,
+                 const char* ISBN, int year);
+void freeBook(Book* b);
+
+// Getters
+const char* getTitle(const Book* b);
+const char* getAuthor(const Book* b);
+const char* getISBN(const Book* b);
+int getYear(const Book* b);
+bool isAvailable(const Book* b);
+
+// Setter
+void setAvailability(Book* b, bool available);
+
+// Display
+void printBook(const Book* b);
 
 #endif
